ace9e7808440b506a270f8c235a0b75abbad4632
LuiisM22 <lm.2295@hotmail.com>
2021-05-22 16:52:10 -0600

    Navigation

--- src/Navigation/Home.js
+++ src/Navigation/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import InternetStatus from './InternetStatus';
-import BatteryStatus from './BatteryStatus';
+import InternetStatus from '../Manager/InternetStatus';
+import BatteryStatus from '../Manager/BatteryStatus';
 
 export default class Home extends Component {
   render() {
