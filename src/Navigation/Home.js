cf9f1536f75a49f921a2bfad7ecad980ab2c19a3
LuiisM22 <lm.2295@hotmail.com>
2021-05-22 16:47:45 -0600

    Navigation

---
+++ src/Navigation/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import InternetStatus from './InternetStatus';
+import BatteryStatus from './BatteryStatus';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Home </Text>
+      </View>
+    );
+  }
+}
