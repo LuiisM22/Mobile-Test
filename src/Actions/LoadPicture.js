daedeb45b6b7b71754d58004a5b646b1f00ce1eb
LuiisM22 <lm.2295@hotmail.com>
2021-05-22 17:01:04 -0600

    Navigation

---
+++ src/Actions/LoadPicture.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class LoadPicture extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Load Picture from Galery </Text>
+      </View>
+    );
+  }
+}
--- src/Navigation/Home.js
+++ src/Navigation/Home.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import InternetStatus from '../Manager/InternetStatus';
 import BatteryStatus from '../Manager/BatteryStatus';
+import TakePicture from '../Actions/TakePicture';
+import LoadPicture from '../Actions/LoadPicture';
 
 export default class Home extends Component {
   render() {
     return (
       <View>
         <Text> Home </Text>
+        <TakePicture />
+        <LoadPicture />
       </View>
     );
   }
